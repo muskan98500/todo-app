cd2a0c397406c5f518d6bf94e062763ea531c0a9
unknown <muskanmahna100@gmail.com>
2022-09-21 16:41:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./App.css";
+;
+import Todo2 from './ProjectDemo/Todo2'
+
+
+
+function App() {
+  return (
+    <div className="App">
+   
+     <Todo2/>
+    </div>
+  );
+}
+
+export default App;
